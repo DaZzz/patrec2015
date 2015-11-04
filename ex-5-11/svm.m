30974d4a71fefb05a2b93b6bd82e25a4c0f00f33
Tikhon Belousko <dazmail01@gmail.com>
2015-11-04 16:57:18 +0200

    Add ex-5-11 folder.

---
+++ ex-5-11/svm.m
@@ -0,0 +1,14 @@
+function [w, w0] = svm(traindata, trainclass, C)
+%%%
+% Simple SVM method for finding boundary
+%
+%     'traindata' - training data.
+%    'trainclass' - classes for train data.
+%             'C' - parameter that controls the penalty associated 
+%                   with an incorrect classification
+%%%
+y = ones(size(trainclass, 2), 1);
+y(trainclass == 2) = -1;
+
+disp(y);
+end
---
+++ ex-5-11/task1.m
@@ -0,0 +1,8 @@
+clc;
+clear all;
+
+load data1.mat;
+load data2.mat;
+load data3.mat;
+
+svm(data1, class1, 1);
