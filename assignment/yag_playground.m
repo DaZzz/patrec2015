a2703da02df049d60edba98e59c147afd6fdfeed
Yagich <yagich@bk.ru>
2015-11-30 20:36:31 +0200

    Add statistic collection for different training set sizes.
    Add multiple experiments selecting subsets of training set of different
    sizes.
    Plot error rate statistics.

--- assignment/yag_playground.m
+++ assignment/yag_playground.m
@@ -42,35 +42,78 @@ end
 
 % Experiment on classifying with different subsets.
 experimentsNumber = 100;
-knnErrorRates = zeros(experimentsNumber, 1);
-randomErrorRates = zeros(experimentsNumber, 1);
-for i = 1:experimentsNumber
-    % Generate training and testing sets.
-    randomSampleOrder  = randperm(samplesNumber);
-    trainingSamplesIDs = randomSampleOrder(1:end/2);
-    testingSamplesIDs  = randomSampleOrder(end/2+1:end);
-    trainingData = data(:,trainingSamplesIDs);
-    trainingClasses = dataClasses(trainingSamplesIDs);
-    testingData = data(:,testingSamplesIDs);
-    testingClasses = dataClasses(testingSamplesIDs);
-    testingSize = length(testingClasses);
+k = 1;
+trainingSizePercentages = 10:10:100;
+trainingSizes = floor(samplesNumber/2*trainingSizePercentages/100);
+trainingSizesNumber = length(trainingSizes);
+meanKnnErrorRates = zeros(trainingSizesNumber, 1);
+meanRandomErrorRates = zeros(trainingSizesNumber, 1);
+knnErrorRateStds = zeros(trainingSizesNumber, 1);
+randomErrorRateStds = zeros(trainingSizesNumber, 1);
+for i = 1:trainingSizesNumber
+    knnErrorRates = zeros(experimentsNumber, 1);
+    randomErrorRates = zeros(experimentsNumber, 1);
+    for j = 1:experimentsNumber
+        % Generate training and testing sets.
+        randomSampleOrder  = randperm(samplesNumber);
+        trainingSamplesIDs = randomSampleOrder(1:trainingSizes(i));
+        testingSamplesIDs  = randomSampleOrder(end/2+1:end);
+        trainingData = data(:,trainingSamplesIDs);
+        trainingClasses = dataClasses(trainingSamplesIDs);
+        testingData = data(:,testingSamplesIDs);
+        testingClasses = dataClasses(testingSamplesIDs);
+        testingSize = length(testingClasses);
 
-    % Classify with knn.
-    knnClasses = knn(trainingClasses, trainingData, testingData, 1);
-    randomClasses = classifyRandomly(trainingClasses, testingData);
-    knnErrorRates(i) = ...
-        length(find(knnClasses ~= testingClasses))/testingSize*100;
-    randomErrorRates(i) = ...
-        length(find(randomClasses ~= testingClasses))/testingSize*100;
+        % Classify with knn.
+        knnClasses = knn(trainingClasses, trainingData, testingData, k);
+        randomClasses = classifyRandomly(trainingClasses, testingData);
+        knnErrorRates(j) = ...
+            length(find(knnClasses ~= testingClasses))/testingSize*100;
+        randomErrorRates(j) = ...
+            length(find(randomClasses ~= testingClasses))/testingSize*100;
+    end
+    meanKnnErrorRates(i) = mean(knnErrorRates);
+    meanRandomErrorRates(i) = mean(randomErrorRates);
+    knnErrorRateStds(i) = std(knnErrorRates);
+    randomErrorRateStds(i) = std(randomErrorRates);
 end
-disp('Knn mean error rate, %:');
-disp(mean(knnErrorRates));
-disp('Random mean error rate, %:');
-disp(mean(randomErrorRates));
-disp('Knn error rate std, %:');
-disp(std(knnErrorRates));
-disp('Random error rate std, %:');
-disp(std(randomErrorRates));
+
+% Error rate boundaries.
+upperKnnErrorRateBand = meanKnnErrorRates + 2*knnErrorRateStds;
+lowerKnnErrorRateBand = max(meanKnnErrorRates - 2*knnErrorRateStds, ...
+    zeros(size(meanKnnErrorRates)));
+upperRandomErrorRateBand = meanRandomErrorRates + 2*randomErrorRateStds;
+lowerRandomErrorRateBand = max(meanRandomErrorRates - ...
+    2*randomErrorRateStds, zeros(size(meanRandomErrorRates)));
+
+% Ranges.
+xmin = min(trainingSizePercentages);
+xmax = max(trainingSizePercentages);
+ymin = 0;
+ymax = ceil(max(max(upperRandomErrorRateBand),...
+    max(upperKnnErrorRateBand))) + 1;
+
+% Plotting.
+subplot(1, 2, 1);
+plot(trainingSizePercentages, meanKnnErrorRates, 'b-',...
+    trainingSizePercentages, knnErrorRateStds, 'r:',...
+    trainingSizePercentages, upperKnnErrorRateBand, 'g--',...
+    trainingSizePercentages, lowerKnnErrorRateBand, 'g--');
+axis([xmin xmax ymin ymax]);
+title('Knn');
+legend('Mean error rate','Error rate std','Error rate band',...
+    'Location', 'best');
+xlabel('Training set size percentage');
+subplot(1, 2, 2);
+plot(trainingSizePercentages, meanRandomErrorRates, 'b-',...
+    trainingSizePercentages, randomErrorRateStds, 'r:',...
+    trainingSizePercentages, upperRandomErrorRateBand, 'g--',...
+    trainingSizePercentages, lowerRandomErrorRateBand, 'g--');
+axis([xmin xmax ymin ymax]);
+title('Random');
+legend('Mean error rate','Error rate std','Error rate band',...
+    'Location', 'best');
+xlabel('Training set size percentage');
 
 % Timer stop.
 toc;
